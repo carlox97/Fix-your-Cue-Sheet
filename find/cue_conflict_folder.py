e17ea66e5f62d6b15b3b5dabdc5d7a039015776d
Carlo <31673755+carlox97@users.noreply.github.com>
2021-12-02 15:12:45 +0100

    Update cue_conflict_folder.py

--- find/cue_conflict_folder.py
+++ find/cue_conflict_folder.py
@@ -12,5 +12,5 @@ for root, dirs, files in os.walk('C:\\whatever\\path'):
         elif f.endswith(".flac") or f.endswith(".ape") or f.endswith(".wv") or f.endswith(".wav") or f.endswith(".wavpack") or f.endswith(".FLAC") or f.endswith(".APE") or f.endswith(".WV") or f.endswith(".WAV") or f.endswith(".WAVPACK") or f.endswith(".Flac"):
             audio_files += 1
 
-    if cue >= 1 or (cue == 1 and audio_files != 1):
+    if cue > 1 or (cue == 1 and audio_files != 1):
         print(root)
