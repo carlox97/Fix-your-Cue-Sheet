3f55bfe13b1d23f8a4398a4e99d25171c924f291
Carlo <31673755+carlox97@users.noreply.github.com>
2023-12-26 01:03:35 +0100

    simpler, more robust

--- cue_conflict_folder.py
+++ cue_conflict_folder.py
@@ -1,17 +1,14 @@
 import os
 
-
-cwd = os.getcwd()
-
-for root, dirs, files in os.walk(cwd):
+for root, dirs, files in os.walk(os.getcwd()):
 
     cue = 0
     audio_files = 0
 
     for f in files:
-        if f.endswith(".cue") or f.endswith(".CUE") or f.endswith(".Cue"):
+        if f.lower().endswith(".cue"):
             cue += 1
-        elif f.endswith(".flac") or f.endswith(".ape") or f.endswith(".wv") or f.endswith(".wav") or f.endswith(".wavpack") or f.endswith(".FLAC") or f.endswith(".APE") or f.endswith(".WV") or f.endswith(".WAV") or f.endswith(".WAVPACK") or f.endswith(".Flac"):
+        elif f.lower().endswith((".flac", ".ape", ".wv", ".wav", ".wavpack")): 
             audio_files += 1
 
     if cue > 1 or (cue == 1 and audio_files != 1):
