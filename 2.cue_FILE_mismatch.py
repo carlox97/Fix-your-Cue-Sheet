c7c8c4d4a0bd8accb6821095b7c9d09e92ee68ff
Carlo <31673755+carlox97@users.noreply.github.com>
2023-12-26 01:52:54 +0100

    cleanup

--- 2.cue_FILE_mismatch.py
+++ 2.cue_FILE_mismatch.py
@@ -3,18 +3,18 @@ from pathlib import Path
 
 
 def cue_action(cue, audio_name, audio_ext):
-   marks = "\""
-   search = bytes(marks.encode()) + bytes(audio_name.encode()) + bytes(audio_ext.encode()) + bytes(marks.encode())
+    marks = "\""
+    search = bytes(marks.encode()) + bytes(audio_name.encode()) + bytes(audio_ext.encode()) + bytes(marks.encode())
 
-   try:
+    try:
        with open(cue, 'rb') as opened_cue:
            for line in opened_cue:
                if search in line:
                   return False
-   except:
+    except:
        print("Couldn't open " + cue)
 
-   return True
+    return True
 
 
 for root, dirs, files in os.walk(os.getcwd()):
@@ -23,15 +23,13 @@ for root, dirs, files in os.walk(os.getcwd()):
     audio_name = None
 
     for f in files:
-        if audio_ext is None and (f.lower().endswith((".flac", ".ape", ".wv", ".wav", ".wavpack"))):    
-            audio_ext = Path(f).suffix
-            audio_name = Path(f).stem
-
-    if audio_ext is not None:
-            for f in files:
-                if f.lower().endswith(".cue"):
-                    cue = os.path.join(root, f)
-                    if cue_action(cue, audio_name, audio_ext):
-                        print(root)
-                        #print(cue)
-                    #break
+        if f.lower().endswith(".cue"):
+            cue = os.path.join(root, f)
+            for g in files:
+                if audio_ext is None and (g.lower().endswith((".flac", ".ape", ".wv", ".wav", ".wavpack"))):    
+                    audio_ext = Path(g).suffix
+                    audio_name = Path(g).stem
+            if audio_name is not None and cue_action(cue, audio_name, audio_ext):
+                print(root)
+                #print(cue)
+                #break
