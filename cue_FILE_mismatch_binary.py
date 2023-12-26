e80bad90f1f5f63ec3f7ab73882af21efec7851e
Carlo <31673755+carlox97@users.noreply.github.com>
2023-12-26 01:07:11 +0100

    simpler and more robust

--- cue_FILE_mismatch_binary.py
+++ cue_FILE_mismatch_binary.py
@@ -3,38 +3,33 @@ from pathlib import Path
 
 
 def cue_action(cue, audio_name, audio_ext):
+   marks = "\""
+   search = bytes(marks.encode()) + bytes(audio_name.encode()) + bytes(audio_ext.encode()) + bytes(marks.encode())
 
-    apici = "\""
+   try:
+       with open(cue, 'rb') as opened_cue:
+           for line in opened_cue:
+               if search in line:
+                  return False
+   except:
+       print("Couldn't open " + cue)
 
-    try:
-        opened_cue = open(cue, 'rb')
-    except:
-        print("Couldn't open " + cue)
-    with opened_cue:
-        search = bytes(apici.encode()) + bytes(audio_name.encode()) + bytes(audio_ext.encode()) + bytes(apici.encode())
-        if search in opened_cue.read():
-            opened_cue.close()
-            return False
-
-        opened_cue.close()
-        return True
+   return True
 
 
-cwd = os.getcwd()
-
-for root, dirs, files in os.walk(cwd):
+for root, dirs, files in os.walk(os.getcwd()):
 
     audio_ext = None
     audio_name = None
 
     for f in files:
-        if audio_ext is None and (f.endswith(".flac") or f.endswith(".ape") or f.endswith(".wv") or f.endswith(".wav") or f.endswith(".wavpack") or f.endswith(".FLAC") or f.endswith(".APE") or f.endswith(".WV") or f.endswith(".WAV") or f.endswith(".WAVPACK") or f.endswith(".Flac")):
+        if audio_ext is None and (f.lower().endswith((".flac", ".ape", ".wv", ".wav", ".wavpack"))):    
             audio_ext = Path(f).suffix
             audio_name = Path(f).stem
 
     if audio_ext is not None:
             for f in files:
-                if f.endswith(".cue") or f.endswith(".CUE") or f.endswith(".Cue"):
+                if f.lower().endswith(".cue"):
                     cue = os.path.join(root, f)
                     if cue_action(cue, audio_name, audio_ext):
                         print(root)
